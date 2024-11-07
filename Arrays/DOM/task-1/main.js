cb9446ba192347d63fe40f596945a31708045d9d
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2024-11-07 19:11:26 +0600

    corrected

--- Arrays/DOM/task-1/main.js
+++ Arrays/DOM/task-1/main.js
@@ -3,9 +3,10 @@ const boxImages2 = document.querySelector(".box-images-2");
 
 boxImage.forEach((boxImage) => {
   boxImage.addEventListener("click", function () {
-    boxImages2.classList.add(".box-image-2");
+    boxImages2.classList.add("box-image-2");
     
     const imageClone = boxImage.cloneNode(true)
     boxImages2.append(imageClone);
+    
   });
 });
