f07c2a1c3c8ed27bebc98c06f0105a592f6ed087
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2024-11-07 18:32:19 +0600

    task-DOM-1

---
+++ Arrays/DOM/task-1/main.js
@@ -0,0 +1,11 @@
+const boxImage = document.querySelectorAll(".box-image");
+const boxImages2 = document.querySelector(".box-images-2");
+
+boxImage.forEach((boxImage) => {
+  boxImage.addEventListener("click", function () {
+    boxImages2.classList.add(".box-image-2");
+    
+    const imageClone = boxImage.cloneNode(true)
+    boxImages2.append(imageClone);
+  });
+});
