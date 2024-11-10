7dda32ab23bd3e2d451533914923c358572c76d1
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2024-11-10 10:49:46 +0600

    corrected task-1, task-3

--- Arrays/DOM/task-1/main.js
+++ Arrays/DOM/task-1/main.js
@@ -1,12 +1,13 @@
-const boxImage = document.querySelectorAll(".box-image");
-const boxImages2 = document.querySelector(".box-images-2");
+const images = document.querySelectorAll(".box-image");
+const boxImages = document.querySelector(".box-images-2");
 
-boxImage.forEach((boxImage) => {
-  boxImage.addEventListener("click", function () {
-    boxImages2.classList.add("box-image-2");
-    
-    const imageClone = boxImage.cloneNode(true)
-    boxImages2.append(imageClone);
-    
+images.forEach((image) => {
+  image.addEventListener("click", function () {
+    boxImages.innerHTML = "";
+
+    const img = document.createElement("img");
+    img.src = image.src;
+
+    boxImages.append(img);
   });
 });
--- Arrays/DOM/task-3/main.js
+++ Arrays/DOM/task-3/main.js
@@ -1,37 +1,33 @@
-const listUl = document.createElement('ul');
+const listUl = document.createElement("ul");
 document.body.append(listUl);
 
-const arr = [100, 300, 250, 700, 500]
+const arr = [100, 300, 250, 700, 500];
 
-function listUlFunc(arr) {
-  listUl.innerHTML = ""
+function renderList(arr) {
+  listUl.innerHTML = "";
   for (let i = 0; i < arr.length; i++) {
-    const listLi = document.createElement('li')
-    listLi.textContent = arr[i]
-    listUl.append(listLi)
-    
+    const listLi = document.createElement("li");
+    listLi.textContent = arr[i];
+    listUl.append(listLi);
   }
 }
 
-listUlFunc(arr)
+renderList(arr);
 
-const buttonFileterIncrease = document.createElement('button')
-buttonFileterIncrease.textContent = 'Сортировать по возрастанию'
-document.body.append(buttonFileterIncrease)
+const buttonFilterIncrease = document.createElement("button");
+buttonFilterIncrease.textContent = "Сортировать по возрастанию";
+document.body.append(buttonFilterIncrease);
 
-const buttonFileterDecrease = document.createElement('button')
-buttonFileterDecrease.textContent = 'Сортировать по убыванию'
-document.body.append(buttonFileterDecrease)
+const buttonFileterDecrease = document.createElement("button");
+buttonFileterDecrease.textContent = "Сортировать по убыванию";
+document.body.append(buttonFileterDecrease);
 
+buttonFilterIncrease.addEventListener("click", function (e) {
+  arr.sort((a, b) => a - b);
+  renderList(arr);
+});
 
-buttonFileterIncrease.addEventListener('click', function(e) {
-  arr.sort((a, b) => a - b)
-  listUlFunc(arr)
-})
-
-buttonFileterDecrease.addEventListener('click', function(e) {
-  arr.sort((a, b) => b - a)
-  listUlFunc(arr)
-})
-
-
+buttonFileterDecrease.addEventListener("click", function (e) {
+  arr.sort((a, b) => b - a);
+  renderList(arr);
+});
