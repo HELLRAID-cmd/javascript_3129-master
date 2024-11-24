9688d467f38b649d900351789ca694f8535ed152
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2024-11-24 23:05:46 +0600

    task-1

--- Arrays/input/task-1/main.js
+++ Arrays/input/task-1/main.js
@@ -1,34 +1,63 @@
 const userName = document.querySelector("#userName");
 const userEmail = document.querySelector('#userEmail')
+
+const userFloorMale = document.querySelector('#male')
+const userFloorFemale = document.querySelector('#female')
+
+const userGrade = document.querySelector('#userGrade')
+const userInterest = document.querySelector('#interest')
+
 const form = document.querySelector("#form");
 
 const buttonSubmit = document.querySelector("button");
 const submittingForm = document.querySelector("#submitting-form");
 
 buttonSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
   if(!userName.value.trim() || !userEmail.value.trim()) {
     e.preventDefault();
     return;
   }
-  submittingForm.innerHTML = ''
-  
-  // Заголовок
+  submittingForm.innerHTML = '';
+
+  //* Заголовок
   const headerText = document.createElement("h2");
   headerText.textContent = "Результаты формы:";
   submittingForm.append(headerText);
 
-  // Имя пользователя
+  //* Имя пользователя
   const nameParagraph = document.createElement("p");
-  nameParagraph.textContent = userName.value;
+  nameParagraph.textContent = `Имя пользователя: ${userName.value}`;
   submittingForm.append(nameParagraph);
 
-  // Email
+  //* Email
   const emailParagraph = document.createElement('p');
-  emailParagraph.textContent = userEmail.value;
+  emailParagraph.textContent = `Email: ${userEmail.value}`;
   submittingForm.append(emailParagraph);
 
-  //grade
+  //* Пол
+  const floorParagraph = document.createElement('p');
+  if(userFloorMale.checked) {
+    floorParagraph.textContent = `Пол: Мужской`;
+    submittingForm.append(floorParagraph);
+  } else if(userFloorFemale.checked) {
+    floorParagraph.textContent = `Пол: Женский`;
+    submittingForm.append(floorParagraph);
+  }
+
+  //* Оценка сервиса
+  const gradeParagraph = document.createElement('p');
+  gradeParagraph.textContent = `Оценка сервиса: ${userGrade.value}`
+  submittingForm.append(gradeParagraph);
+
+  //* Интересы пользователей
+  const interestParagraph = document.createElement('p');
+  interestParagraph.textContent = `Интересы пользователей: ${userInterest.value}`;
+  submittingForm.append(interestParagraph);
 
   userName.value = "";
   userEmail.value = "";
+  userFloorMale.checked = false;
+  userFloorFemale.checked = false;
+  userGrade.value = "";
 });
