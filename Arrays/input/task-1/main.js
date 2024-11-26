d8d1d8936f10fc8b3ddc747a594136e59433a9b6
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2024-11-26 10:39:13 +0600

    complete task-1

--- Arrays/input/task-1/main.js
+++ Arrays/input/task-1/main.js
@@ -1,11 +1,12 @@
 const userName = document.querySelector("#userName");
-const userEmail = document.querySelector('#userEmail')
+const userEmail = document.querySelector("#userEmail");
 
-const userFloorMale = document.querySelector('#male')
-const userFloorFemale = document.querySelector('#female')
+const userFloorMale = document.querySelector("#male");
+const userFloorFemale = document.querySelector("#female");
 
-const userGrade = document.querySelector('#userGrade')
-const userInterest = document.querySelector('#interest')
+const userGrade = document.querySelector("#userGrade");
+const userInterest = document.querySelectorAll("input[name=interest]");
+const userTextarea = document.querySelector("#textarea");
 
 const form = document.querySelector("#form");
 
@@ -14,11 +15,11 @@ const submittingForm = document.querySelector("#submitting-form");
 
 buttonSubmit.addEventListener("click", function (e) {
   e.preventDefault();
-  if(!userName.value.trim() || !userEmail.value.trim()) {
+  if (!userName.value.trim() || !userEmail.value.trim()) {
     e.preventDefault();
     return;
   }
-  submittingForm.innerHTML = '';
+  submittingForm.innerHTML = "";
 
   //* Заголовок
   const headerText = document.createElement("h2");
@@ -31,33 +32,49 @@ buttonSubmit.addEventListener("click", function (e) {
   submittingForm.append(nameParagraph);
 
   //* Email
-  const emailParagraph = document.createElement('p');
+  const emailParagraph = document.createElement("p");
   emailParagraph.textContent = `Email: ${userEmail.value}`;
   submittingForm.append(emailParagraph);
 
   //* Пол
-  const floorParagraph = document.createElement('p');
-  if(userFloorMale.checked) {
+  const floorParagraph = document.createElement("p");
+  if (userFloorMale.checked) {
     floorParagraph.textContent = `Пол: Мужской`;
     submittingForm.append(floorParagraph);
-  } else if(userFloorFemale.checked) {
+  } else if (userFloorFemale.checked) {
     floorParagraph.textContent = `Пол: Женский`;
     submittingForm.append(floorParagraph);
   }
 
   //* Оценка сервиса
-  const gradeParagraph = document.createElement('p');
-  gradeParagraph.textContent = `Оценка сервиса: ${userGrade.value}`
+  const gradeParagraph = document.createElement("p");
+  gradeParagraph.textContent = `Оценка сервиса: ${userGrade.value}`;
   submittingForm.append(gradeParagraph);
 
   //* Интересы пользователей
-  const interestParagraph = document.createElement('p');
-  interestParagraph.textContent = `Интересы пользователей: ${userInterest.value}`;
+  let selectedInterest = [];
+  userInterest.forEach((checkbox) => {
+    if (checkbox.checked) {
+      selectedInterest.push(checkbox.value);
+    }
+  });
+
+  const interestParagraph = document.createElement("p");
+  interestParagraph.textContent = `Интересы пользователей: ${selectedInterest.join(
+    ", "
+  )}`;
   submittingForm.append(interestParagraph);
+
+  //*Дополнительные комментарии
+  const commentsParagraph = document.createElement("p");
+  commentsParagraph.textContent = `Дополнительные комментраии: ${userTextarea.value}`;
+  submittingForm.append(commentsParagraph);
 
   userName.value = "";
   userEmail.value = "";
   userFloorMale.checked = false;
   userFloorFemale.checked = false;
   userGrade.value = "";
+  userInterest.forEach((checbox) => (checbox.checked = false));
+  userTextarea.value = "";
 });
