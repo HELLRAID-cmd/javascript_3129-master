d57cf9457761f5eb96fb09e014c2baa27a459b0a
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2024-11-28 16:38:29 +0600

    format

--- Arrays/input/task-3/main,.js
+++ Arrays/input/task-3/main,.js
@@ -1,46 +1,46 @@
-const form = document.querySelector('.form');
-const cardText = document.querySelector('#cardText');
+const form = document.querySelector(".form");
+const cardText = document.querySelector("#cardText");
 
-const card = document.querySelector('.card');
+const card = document.querySelector(".card");
 
-form.addEventListener('submit', function(event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   card.innerHTML = "";
 
-  const title = document.createElement('h2');
-  title.textContent = 'Результат:';
+  const title = document.createElement("h2");
+  title.textContent = "Результат:";
   card.append(title);
 
-
   //*Карточка
-  const cardWrapper = document.createElement('div');
-  cardWrapper.classList.add('card__wrapper');
+  const cardWrapper = document.createElement("div");
+  cardWrapper.classList.add("card__wrapper");
   card.append(cardWrapper);
 
-
   //*Текст карты
-  const cardContent = document.createElement('p');
+  const cardContent = document.createElement("p");
   cardContent.textContent = cardText.value;
-  cardContent.classList.add('card__wrapper-desc');
+  cardContent.classList.add("card__wrapper-desc");
   cardWrapper.append(cardContent);
 
   //*Цвет карты
   const selectedColor = document.querySelector('input[name="color"]:checked');
   if (selectedColor) {
-    cardWrapper.style.backgroundColor = selectedColor.id.replace('color-', '');
+    cardWrapper.style.backgroundColor = selectedColor.id.replace("color-", "");
   }
 
   card.append(cardWrapper);
 
   cardText.value = "";
-  document.querySelectorAll('input[name="color"]').forEach((radio) => (radio.checked = false));
-})
-
-cardText.addEventListener('focus', function() {
-  cardText.style.backgroundColor = 'aqua'; 
+  document
+    .querySelectorAll('input[name="color"]')
+    .forEach((radio) => (radio.checked = false));
 });
 
-cardText.addEventListener('blur', function() {
-  cardText.style.backgroundColor = ''; 
+cardText.addEventListener("focus", function () {
+  cardText.style.backgroundColor = "aqua";
+});
+
+cardText.addEventListener("blur", function () {
+  cardText.style.backgroundColor = "";
 });
