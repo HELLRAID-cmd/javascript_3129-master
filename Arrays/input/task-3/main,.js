b203226744e8ffabe3ab9228c0d717edcfe3cf2b
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2024-12-01 19:38:08 +0600

    task-3

--- Arrays/input/task-3/main,.js
+++ Arrays/input/task-3/main,.js
@@ -1,40 +1,13 @@
-const form = document.querySelector(".form");
 const cardText = document.querySelector("#cardText");
-
 const card = document.querySelector(".card");
+const select = document.querySelector('#select');
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
+cardText.addEventListener('input', function() {
+  card.innerText = cardText.value;
+});
 
-  card.innerHTML = "";
-
-  const title = document.createElement("h2");
-  title.textContent = "Результат:";
-  card.append(title);
-
-  //*Карточка
-  const cardWrapper = document.createElement("div");
-  cardWrapper.classList.add("card__wrapper");
-  card.append(cardWrapper);
-
-  //*Текст карты
-  const cardContent = document.createElement("p");
-  cardContent.textContent = cardText.value;
-  cardContent.classList.add("card__wrapper-desc");
-  cardWrapper.append(cardContent);
-
-  //*Цвет карты
-  const selectedColor = document.querySelector('input[name="color"]:checked');
-  if (selectedColor) {
-    cardWrapper.style.backgroundColor = selectedColor.id.replace("color-", "");
-  }
-
-  card.append(cardWrapper);
-
-  cardText.value = "";
-  document
-    .querySelectorAll('input[name="color"]')
-    .forEach((radio) => (radio.checked = false));
+select.addEventListener('change', function() {
+  card.style.backgroundColor = select.value;
 });
 
 cardText.addEventListener("focus", function () {
