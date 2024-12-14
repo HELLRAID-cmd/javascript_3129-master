05dbac11a990391bb4b21a822579492fd32f007e
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2024-12-14 14:02:01 +0600

    complete cookie-1

---
+++ Arrays/cookie/cookie-1/main.js
@@ -0,0 +1,11 @@
+const card = document.querySelector('.card');
+const cardHidden = document.querySelector('.card--hidden');
+const cardButton = document.querySelector('.card-btn');
+
+setTimeout(() => {
+  card.classList.remove('card--hidden');
+}, 3000);
+
+cardButton.onclick = function() { //мог сделать через addEventListener, но решил вспомнить onclick
+  card.classList.add('card--hidden');
+};
