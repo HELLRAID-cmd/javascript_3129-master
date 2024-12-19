253b92d3182e032991580c50f8196112ac96f6a9
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2024-12-19 19:19:55 +0600

    cookie-1

--- Arrays/cookie/cookie-1/main.js
+++ Arrays/cookie/cookie-1/main.js
@@ -1,11 +1,39 @@
 const card = document.querySelector('.card');
 const cardHidden = document.querySelector('.card--hidden');
 const cardButton = document.querySelector('.card-btn');
+const cardIcon = document.querySelector('.card-icon');
+const cardTitle = document.querySelector('.card-title');
+
+const giftArr = [
+  {
+    title: "Скидка 20% на первую покупку в нашем магазине!",
+    icon: "img/discount.svg"
+  },
+  {
+    title: "Скидка 10% на всё!",
+    icon: "img/discount_2.svg"
+  },
+  {
+    title: "Подарок при первой покупке в нашем магазине!",
+    icon: "img/gift.svg"
+  },
+  {
+    title: "Бесплатная доставка для вас!",
+    icon: "img/delivery.svg"
+  },
+  {
+    title: "Сегодня день больших скидок!",
+    icon: "img/discount_3.svg"
+  }
+];
 
 setTimeout(() => {
+  const randomGift = giftArr[Math.floor(Math.random() * giftArr.length)];
+  cardTitle.textContent = randomGift.title;
+  cardIcon.src = randomGift.icon;
   card.classList.remove('card--hidden');
 }, 3000);
 
-cardButton.onclick = function() { // мог сделать через addEventListener, но решил вспомнить onclick
+cardButton.onclick = function() { 
   card.classList.add('card--hidden');
 };
